05c0794c9aa75bffba492cc01bf610e7ff71ad9f
Maurice Montag <rmontag@uw.edu>
2019-08-27 10:51:43 -0700

    Updated using directives to match the new dlls and naming conventions of the function generator interface.

--- UnifiedTestSuiteApp/MainWindow.xaml.cs
+++ UnifiedTestSuiteApp/MainWindow.xaml.cs
@@ -1,16 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 using NationalInstruments.Visa;
 using OscilloscopeAPI;
@@ -20,7 +15,7 @@ using OxyPlot.Series;
 using System.IO;
 using System.Threading;
 using System.Timers;
-using TestSuiteInterface;
+using FunctionGeneratorAPI;
 using Microsoft.Win32;
 using System.Text.RegularExpressions;
 
