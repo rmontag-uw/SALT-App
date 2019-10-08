a8b98d4c5b61b52b0f64bf7d26ecd28e492f4f05
Maurice Montag <rmontag@uw.edu>
2019-10-08 14:27:00 -0700

    Use partial classes to make application code easier to read

---
+++ UnifiedTestSuiteApp/MainWindow_FunctionGeneratorUiHandlers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using System.IO;
+using System.Threading;
+using System.Timers;
+using Microsoft.Win32;
+using System.Text.RegularExpressions;
+using System.Windows.Media.Imaging;
+using TestingPlatformLibrary.FunctionGeneratorAPI;
+using TestingPlatformLibrary.OscilloscopeAPI;
+
+namespace UnifiedTestSuiteApp
+{
+    public partial class MainWindow : Window
+    {
+        
+    }
+}
