49c625c76729a4a1b405e337f8ab95ff1807affb
Maurice Montag <rmontag@uw.edu>
2019-10-17 18:48:47 -0700

    Renamed App

--- UnifiedTestSuiteApp/MainWindow_GlobalObjects.cs
+++ UnifiedTestSuiteApp/MainWindow_GlobalObjects.cs
@@ -15,7 +15,7 @@ namespace UnifiedTestSuiteApp
     /// </summary>
     public partial class MainWindow : Window
     {
-        private const string appName = "Test Suite";
+        private const string appName = "S.A.L.T Application";
         private const int refreshInterval = 150;            // the graph refresh interval in ms
         private static System.Timers.Timer refreshTimer;    // gotta make sure there's no ambiguity with the threading timer
         private readonly IOscilloscope scope;               // 
